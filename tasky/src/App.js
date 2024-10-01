9da660d59a3348b6f9447bffa95279aa6fa6b373
piotrpdev <piotrpdev@gmail.com>
2024-10-01 13:53:54 +0100

    Delete a task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -17,6 +17,12 @@ function App() {
     setTaskState({tasks});
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  }
   
   return (
     <div className="container">
@@ -29,6 +35,7 @@ function App() {
           key={task.id}
           done={task.done}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
         />
       ))}
     </div>
